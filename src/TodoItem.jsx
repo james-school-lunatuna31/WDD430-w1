2507ecc19443fd188732540479965c32f01098d6
James Green <github@ckplace.org>
2024-06-12 22:21:39 -0400

    w1: Aquire style guide from github repo of tutorial and merge any changes needed to repair mistakes

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,18 @@
-export function TodoItem({completed, id, title, toggleTodo, deleteTodo}) {
-    return <li><label><input type="checkbox" checked={completed} onChange={e => toggleTodo(id, e.target.checked)}/>
-      {title}</label>
-      <button className="btn btn-danger" onClick={() => deleteTodo(id)}>Delete</button></li>
-}
+import React from "react"
+export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
+    return (
+      <li>
+        <label>
+          <input
+            type="checkbox"
+            checked={completed}
+            onChange={e => toggleTodo(id, e.target.checked)}
+          />
+          {title}
+        </label>
+        <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+          Delete
+        </button>
+      </li>
+    )
+  }
