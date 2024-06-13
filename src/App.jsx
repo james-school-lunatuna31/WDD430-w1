5d8a9baa011cb9710262893284d85fc7053a6850
James Green <github@ckplace.org>
2024-06-12 22:14:06 -0400

    w1: seperate app into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import { TodoList } from "./TodoList"
 import "./styles.css"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  })
 
-  function handleSubmit(e){
-    e.preventDefault()
+  useEffect(() => {
+    localStorage.setItem("ITEMS",JSON.stringify(todos))
+  }, [todos])
 
-    setTodos((currentTodos) => {
-      return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},]
-    })
-
-    setNewItem("")
+  function addTodo(title) {
+            setTodos((currentTodos) => {
+          return [...currentTodos, {id: crypto.randomUUID(), title, completed: false},]
+        })
   }
 
   function toggleTodo(id, completed){
@@ -32,21 +37,8 @@ export default function App() {
     })
   }
   return <>
-  <form onSubmit = {handleSubmit} className="new-item-form">
-    <div className="form-row">
-      <label htmlFor="item">New Item</label>
-      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
-    </div>
-    <button className="ntn">Add</button>
-  </form>
+  <NewTodoForm onSubmit={addTodo} />
   <h1 className="header">Todo List</h1>
-  <ul className="list">
-    {todos.length === 0 && "No Todos"}
-    {todos.map((todo => {
-      return <li key={todo.id}><label><input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)}/>
-      {todo.title}</label>
-      <button className="btn btn-danger" onClick={() => deleteTodo(todo.id)}>Delete</button></li>
-    }))}
-  </ul>
+ <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
   </>
 }
